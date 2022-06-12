3ac426360045410a2ee0f694369ec0a87c9bfa0d
Avery <averysumner@gmail.com>
2022-06-12 02:11:32 -0700

    formatting fixes

--- tests/test_gui.cc
+++ tests/test_gui.cc
@@ -1,6 +1,6 @@
 // codeshaunted - soul
 // tests/test_gui.cc
-// GUI test source file
+// gui test source file
 // Copyright 2022 the soul team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissionsand
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 #include "doctest.h"
