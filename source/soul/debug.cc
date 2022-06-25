34bf3bc9dbc7d6c97a5344de7069011930685ded
c34a <theo@seelye.net>
2022-06-25 15:56:43 -0700

    implement debug log for text command

--- source/soul/debug.cc
+++ source/soul/debug.cc
@@ -17,5 +17,17 @@ void DrawCmd::Rect::debugLog() {
 
 void DrawCmd::Text::debugLog() {
 	std::cout
-		<< "[Text <todo>]" << std::endl;
+		<< "[Text (" << this->x << "," << this->y << ") s" << this->scale;
+	if (!this->first) {
+		std::cout << " {empty}";
+	} else {
+		auto cur = this->first;
+		while (cur) {
+			std::cout << std::endl << "  "
+				<< std::hex << cur->color_abgr << " \""
+				<< cur->text << "\"";
+			cur = cur->next;
+		}
+	}
+	std::cout << std::endl << "]" << std::endl;
 }
